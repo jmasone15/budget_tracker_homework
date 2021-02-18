b6423b231950a6bb8def6a47211c716808d70fd4
Jordan Masone <jordanmasone1@gmail.com>
2021-02-18 09:59:53 -0500

    Deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,11 +23,15 @@ mongoose.connect(
     useCreateIndex: true,
     useFindAndModify: false
   }
-);
+).then(() => {
+  console.log("Database connected!");
+}).catch((err) => {
+  console.log(err);
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port http://localhost:${PORT}`);
+  console.log(`App running on http://localhost:${PORT}`);
 });
