d7d471a8198fc5c92e6e6c10f2ee1b8208f05c6f
Jordan Masone <jordanmasone1@gmail.com>
2021-02-17 19:04:58 -0500

    Added offline functionality to the app.

---
+++ public/db.js
@@ -0,0 +1,53 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrememt: true });
+};
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatbase();
+    }
+};
+
+request.onerror = (event) => {
+    console.log(`Whoops! ${event.target.errorCode}`);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
